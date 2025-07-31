46d07b9de8c7335fa8ee6c335e137319b87c34a8
ViniciusOLucio <vinicius.olucio@gmail.com>
2025-07-31 09:34:44 -0300

    Add: new style page Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,27 +1,27 @@
-import Header from "../../components/Header";
-import Footer from "../../components/Footer";
 import Banner from "../../components/Banner";
 import Hero from "../../components/Hero";
 import Card from "../../components/Card";
+import videos from 'json/db.json';
+import styles from './Home.module.css';
+import Container from "../../components/Container";
 
 function Home() {
     return (
         <div>
-            <Header/>
+
             <Banner img="home"/>
+            <Container>
             <Hero>
                 <h1>
                     Um lugar para guardar seus vídeos e filmes!
                 </h1>
             </Hero>
-            <Card
-                id="1"
-                title="gato"
-                thumb="https://caelum-online-public.s3.amazonaws.com/2802-react-praticando/img2.png"
-            >
-
-            </Card>
-            <Footer/>
+                <section className={styles.container}>
+                    {videos.map((video => {
+                        return <Card {...video} key={video.id}/>
+                    }))}
+                </section>
+            </Container>
         </div>
     )
 }
