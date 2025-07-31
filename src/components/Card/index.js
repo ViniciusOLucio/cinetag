f4407ff36c8ce520220f2e6d8b6ba37d402e7056
ViniciusOLucio <vinicius.olucio@gmail.com>
2025-07-31 09:33:55 -0300

    Add: context favorite card

--- src/components/Card/index.js
+++ src/components/Card/index.js
@@ -1,22 +1,32 @@
+import { useFavoriteContext } from "../../Context/Favoritos";
 import styles from './Card.module.css';
 import iconeFavoritar from './favoritar.png';
-function Card({ id, title, thumb}){
-    return(
-        <div className={styles.card}>
+import iconeDesfavoritar from './desfavoritar.png';
+
+function Card({ id, title, thumb }) {
+    const { favorito, adicionarFavorito } = useFavoriteContext();
+    const ehFavorito = favorito.some((fav) => fav.id === id);
+    const icone = !ehFavorito ? iconeFavoritar : iconeDesfavoritar;
+
+    return (
+        <div className={styles.card}
+             onClick={() => {
+                 adicionarFavorito({ id, title, thumb });
+             }}>
             <img
                 src={thumb}
                 alt={title}
                 className={styles.capa}
             />
-            <h2>
-                {title}
-            </h2>
+            <h2>{title}</h2>
             <img
-                src={iconeFavoritar}
+                src={icone}
                 alt="favoritar"
                 className={styles.favoritar}
+
             />
         </div>
-    )
+    );
 }
+
 export default Card;
