a1bae021b7d3a85db04a8327ded5da47647b82f1
ViniciusOLucio <vinicius.olucio@gmail.com>
2025-07-31 09:35:06 -0300

    Add: context favorite

--- src/routes.js
+++ src/routes.js
@@ -1,18 +1,23 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
+import Favoritos from "./pages/Favoritos";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import FavoritosProvider from "./Context/Favoritos";
 
-function AppRoutes(){
-    return(
-        <BrowserRouter>
-            <Routes>
-                <Route
-                    path="/"
-                    element={<Home />}
-                >
-                </Route>
-            </Routes>
-        </BrowserRouter>
-    )
+function AppRoutes() {
+    return (
+        <FavoritosProvider>
+            <BrowserRouter>
+                <Header />
+                <Routes>
+                    <Route path="/" element={<Home />} />
+                    <Route path="/favoritos" element={<Favoritos />} />
+                </Routes>
+                <Footer />
+            </BrowserRouter>
+        </FavoritosProvider>
+    );
 }
 
 export default AppRoutes;
